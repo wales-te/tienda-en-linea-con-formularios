d5b6570d2dd3aebbcf16859c9c7cbca86fa1ce36
Jessie Waleska Tejeda Carcamo <waleteca@gmail.com>
2025-10-10 17:09:07 -0600

    Fix: Vite requiere index.html en raíz + config Vercel SPA

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
-  esbuild: {
-    drop: ['console', 'debugger'],
-  },
+  esbuild: { drop: ['console', 'debugger'] }
 })
